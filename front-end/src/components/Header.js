7a525c16f7a8a4b05886e806d2ba50603e19f1d9
rk <renkiat.gan@corpxervices.com>
2022-04-22 16:13:10 +0800

    .

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -4,16 +4,9 @@ class Header extends Component {
     render() {
         return (
             <div className="text-center">
-                {/* <img
-                    src="https://logrocket-assets.io/img/logo.png"
-                    width="300"
-                    className="img-thumbnail"
-                    style={{ marginTop: '20px' }}
-                /> */}
+                
+                <h5>Navbar goes here</h5>
                 <hr />
-                {/* <h5>
-                    <i>presents</i>
-                </h5> */}
                 <h1>App with React + Django</h1>
             </div>
         )
