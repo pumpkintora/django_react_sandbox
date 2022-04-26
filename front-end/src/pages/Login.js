db2843271441deec5d8617d5b1934d22de939c41
rk <renkiat.gan@corpxervices.com>
2022-04-26 09:22:31 +0800

    debug sign in

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -8,6 +8,7 @@ import AuthLayout from '../components/authentication/AuthLayout';
 import Page from '../components/Page';
 import LoginForm from '../components/authentication/LoginForm';
 import AuthSocial from '../components/authentication/AuthSocial';
+import LoginPNG from '../static/illustration_login.png'
 
 // ----------------------------------------------------------------------
 
@@ -52,7 +53,7 @@ export default function Login() {
         <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
           Hi, Welcome Back
         </Typography>
-        <img src="/static/illustrations/illustration_login.png" alt="login" />
+        <img src={LoginPNG} alt="login" />
       </SectionStyle>
 
       <Container maxWidth="sm">
